1afba4af9145f13ad110ebece5e8f5b1405ca799
gautam-fosserp <gautam@fosserp.com>
2026-08-17 19:19:29 +0530

    create purchase invoice from taxi doctype

--- harro/harro/doctype/taxi/taxi.py
+++ harro/harro/doctype/taxi/taxi.py
@@ -20,18 +20,53 @@ class Taxi(Document):
 
 @frappe.whitelist()
 def create_purchase_invoice(source_name, target_doc=None):
-	doclist = get_mapped_doc(
-		"Taxi",
-		source_name,
-		{
-			"Taxi": {
-				"doctype": "Purchase Invoice",
-				"field_map": {
-					"name": "taxi",
-					"custom_supplier": "supplier"
-				}
-			},
-		},
-		target_doc
-	)
-	return doclist
+
+    def update_item_price(item_code, rate):
+        """Update existing Item Price(s) for this item to match invoice rate."""
+        existing_prices = frappe.get_all(
+            "Item Price",
+            filters={"item_code": item_code, "buying": 1},
+            fields=["name", "price_list"],
+        )
+
+        if existing_prices:
+            for ip in existing_prices:
+                frappe.db.set_value("Item Price", ip.name, "price_list_rate", rate)
+
+    def set_missing_values(source, target):
+        item = frappe.get_cached_doc("Item", source.custom_service_item)
+        rate = source.cost
+
+        # update item price BEFORE building the item row, so the client
+        # doesn't overwrite the rate when the form loads
+        update_item_price(item.name, rate)
+
+        target.append("items", {
+            "item_code": source.custom_service_item,
+            "item_name": item.item_name,
+            "uom": item.purchase_uom or item.stock_uom,
+            "stock_uom": item.stock_uom,
+            "conversion_factor": 1,
+            "qty": 1,
+            "rate": rate,
+            "price_list_rate": rate,
+            "cost_center": "Main - Harro IN",
+        })
+
+    doclist = get_mapped_doc(
+        "Taxi",
+        source_name,
+        {
+            "Taxi": {
+                "doctype": "Purchase Invoice",
+                "field_map": {
+                    "name": "taxi",
+                    "custom_taxi_vendor": "supplier",
+                    "custom_bill_no": "bill_no",
+                },
+            },
+        },
+        target_doc,
+        postprocess=set_missing_values,
+    )
+    return doclist
